6be31c01dd322e30e646028642d013943c5caa1f
henrysetiadi <henrysetiadi92@gmail.com>
2019-04-29 10:52:33 +0700

    update file product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,7 +3,7 @@
 </<!DOCTYPE html>
 <html>
 <head>
-    <title>Bootstrap Product</title>
+    <title>Cars Product</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
@@ -37,6 +37,7 @@
           margin-left: 0px !important;
           margin-right: 0px !important;
           margin-top: 10px !important;
+          margin-bottom: 10px !important;
         }
 
         
