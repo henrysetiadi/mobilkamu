18558cd85e9f6562c5b9f6f11d5f361acf0d56d1
henrysetiadi <henrysetiadi92@gmail.com>
2019-05-06 10:10:26 +0700

    tesMK

--- resources/views/uploadfile.blade.php
+++ resources/views/uploadfile.blade.php
@@ -48,7 +48,7 @@
                                 </div>
                                 <div class="form-group col-lg-6 col-md-6 col-sm-12">
                                     <label>Price</label>
-                                    <input type="text" id="price" name="price" value="{{ old('price') }}" class="form-control" autocomplete="off">
+                                    <input type="text" id="price" name="price" value="{{ old('price') }}" class="form-control" autocomplete="off" onkeypress="return hanyaAngka(event)">
                                 </div>
                             </div>
                             <hr/>
@@ -59,17 +59,17 @@
                             <div class="row">
                                 <div class="form-group col-lg-4 col-md-6 col-sm-12">
                                     <label>Length</label>
-                                    <input type="text" id="length" name="length" value="{{ old('length') }}" placeholder="in centimeters" class="form-control" autocomplete="off"> 
+                                    <input type="text" id="length" name="length" value="{{ old('length') }}" placeholder="in centimeters" class="form-control" autocomplete="off" onkeypress="return hanyaAngka(event)"> 
                                 </div>
 
                                 <div class="form-group col-lg-4 col-md-6 col-sm-12">
                                     <label>Width</label>
-                                    <input type="text" id="width" name="width" value="{{ old('width') }}" placeholder="in centimeters" class="form-control" autocomplete="off"> 
+                                    <input type="text" id="width" name="width" value="{{ old('width') }}" placeholder="in centimeters" class="form-control" autocomplete="off" onkeypress="return hanyaAngka(event)"> 
                                 </div>
 
                                 <div class="form-group col-lg-4 col-md-6 col-sm-12">
                                     <label>Height</label>
-                                    <input type="text" id="height" name="height" value="{{ old('price') }}" placeholder="in centimeters" class="form-control" autocomplete="off">
+                                    <input type="text" id="height" name="height" value="{{ old('price') }}" placeholder="in centimeters" class="form-control" autocomplete="off" onkeypress="return hanyaAngka(event)">
                                 </div>
                             </div>
                             <hr/>
@@ -85,4 +85,14 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function hanyaAngka(evt) {
+          var charCode = (evt.which) ? evt.which : event.keyCode
+           if (charCode > 31 && (charCode < 48 || charCode > 57))
+ 
+            return false;
+          return true;
+        }
+    </script>
 @endsection
